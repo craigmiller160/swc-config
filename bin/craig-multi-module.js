5143e5e85a8c32bad333f64355ec8c232a8476ba
Craig Miller <craigmiller160@gmail.com>
2023-07-23 21:34:13 -0400

    Refactored scripts

--- bin/craig-multi-module.js
+++ bin/craig-multi-module.js
@@ -4,6 +4,9 @@ const path = require('path');
 const spawn = require('cross-spawn');
 
 const swcrcPath = path.join(__dirname, '..', '.swcrc');
+const cjsPath = path.join(process.cwd(), 'cjs');
+const esmPath = path.join(process.cwd(), 'esm');
+const srcPath = path.join(process.cwd(), 'src');
 
-spawn.sync('npx', ['swc', '-d', './src', '--config-file', swcrcPath, '-C', 'module.type', 'es6']);
-spawn.sync('npx', ['swc', '-d', './src', '--config-file', swcrcPath, '-C', 'module.type', 'commonjs']);
+spawn.sync('npx', ['swc', srcPath, '-d', esmPath, '--config-file', swcrcPath, '-C', 'module.type', 'es6']);
+spawn.sync('npx', ['swc', srcPath, '-d', cjsPath, '--config-file', swcrcPath, '-C', 'module.type', 'commonjs']);
